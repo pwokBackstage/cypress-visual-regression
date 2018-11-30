f25c0603e0f4738035cc797167bcb0d57986e219
Patrick Walker <patrick.walker@teamwork.com>
2018-11-30 16:59:52 +0000

    creating diff folder if missing

--- src/plugin.js
+++ src/plugin.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const fs = require('fs');
 const BlinkDiff = require('blink-diff');
 
 // TODO: allow user to define/update
@@ -11,14 +11,21 @@ const SCREENSHOT_DIRECTORY = process.env.SCREENSHOT_DIRECTORY || path.join(
   __dirname, '..', '..', '..', 'cypress', 'screenshots',
 );
 
+
 function compareSnapshotsPlugin(args) {
+  const DIFF_DIRECTORY = process.env.DIFF_DIRECTORY || path.join(
+    SNAPSHOT_DIRECTORY, 'diff', args.specDirectory,
+  );
+  if (!fs.existsSync(DIFF_DIRECTORY)) {
+    fs.mkdirSync(DIFF_DIRECTORY);
+  }
   return new Promise((resolve, reject) => {
     const diff = new BlinkDiff({
       imageAPath: path.join(SNAPSHOT_DIRECTORY, 'base', args.specDirectory, `${args.fileName}.png`), // Use file-path
       imageBPath: path.join(SCREENSHOT_DIRECTORY, args.specDirectory, `${args.fileName}.png`),
       thresholdType: BlinkDiff.THRESHOLD_PERCENT,
       threshold: 0.001, // 1% threshold
-      imageOutputPath: path.join(SNAPSHOT_DIRECTORY, 'diff', args.specDirectory, `${args.fileName}.png`),
+      imageOutputPath: path.join(DIFF_DIRECTORY, `${args.fileName}.png`),
     });
 
     diff.run((error, result) => {
