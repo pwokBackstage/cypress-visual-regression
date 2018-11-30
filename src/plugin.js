8481a208863249a5ba8e1b7c7b7b7bce5de4f367
Patrick Walker <patrick.walker@teamwork.com>
2018-11-30 15:49:28 +0000

    Adding a new screenshot directory

--- src/plugin.js
+++ src/plugin.js
@@ -7,11 +7,15 @@ const SNAPSHOT_DIRECTORY = process.env.SNAPSHOT_DIRECTORY || path.join(
   __dirname, '..', '..', '..', 'cypress', 'snapshots',
 );
 
+const SCREENSHOT_DIRECTORY = process.env.SCREENSHOT_DIRECTORY || path.join(
+  __dirname, '..', '..', '..', 'cypress', 'screenshots',
+);
+
 function compareSnapshotsPlugin(args) {
   return new Promise((resolve, reject) => {
     const diff = new BlinkDiff({
       imageAPath: path.join(SNAPSHOT_DIRECTORY, 'base', args.specDirectory, `${args.fileName}.png`), // Use file-path
-      imageBPath: path.join(SNAPSHOT_DIRECTORY, args.specDirectory, `${args.fileName}.png`),
+      imageBPath: path.join(SCREENSHOT_DIRECTORY, args.specDirectory, `${args.fileName}.png`),
       thresholdType: BlinkDiff.THRESHOLD_PERCENT,
       threshold: 0.1, // 1% threshold
       imageOutputPath: path.join(SNAPSHOT_DIRECTORY, 'diff', args.specDirectory, `${args.fileName}.png`),
